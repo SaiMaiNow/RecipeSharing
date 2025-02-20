cb68ec22af540ae40b590ca33a8356b68903d0be
SaiMaiNow <commanchannel@gmail.com>
2025-02-21 02:59:06 +0700

    setup project nextjs manual

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Page() {
+    return <>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold">Hello, Next.js!</h1>
+            <p className="text-2xl">This is a test</p>
+        </div>
+    </>
+}
